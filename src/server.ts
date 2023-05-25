6ef5e852868ee4283de56465b228d96755727227
Ilya Sher <34807039+ilyash-b@users.noreply.github.com>
2023-05-25 12:00:41 +0400

    Improve DX - auto authentication

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
+import {exec} from 'child_process';
+
 import * as http from 'http';
 import * as net from 'net';
+import {AddressInfo} from 'net';
 import * as crypto from "crypto";
-
-import express = require('express');
 import * as WebSocket from 'ws';
+import express = require('express');
 import debug0 = require('debug');
-
-import { AddressInfo } from 'net';
+import * as assert from "assert";
 
 const debug = debug0('ngs-web-ui-proxy');
 
@@ -16,12 +20,44 @@ const wss = new WebSocket.Server({ server });
 
 console.log('argv', process.argv);
 
+// Auth - start setup
+// Authentication is a mess, redo later
+const secret = crypto.randomBytes(20).toString('hex');
+const secretFilePath = path.join(os.homedir(), '.ngs-ui-secret');
+fs.writeFileSync(secretFilePath, secret, {mode: 0o600});
+console.log('Secret file is at', secretFilePath);
+let canAuthenticate = true; // Single use secret
+let authAttempts = 0;
+
+setTimeout(function () {
+    canAuthenticate = false;
+    console.log('Authentication is now disabled');
+}, 10000);
+// Auth - end setup
+
+// TODO: open the URL in the browser
+// http://localhost:3000/?secret=XX
+
+function runCommand(command: string): void {
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error executing command: ${error}`);
+            return;
+        }
+        console.log(`Command output:\n${stdout}`);
+        if (stderr) {
+            console.error(`Command error output:\n${stderr}`);
+        }
+    });
+}
+
+runCommand(`open 'http://localhost:3000/?secret=${encodeURIComponent(secret)}'`);
+
+
 wss.on('connection', (ws: WebSocket) => {
 
-    let code = crypto.randomBytes(20).toString('hex');
-    console.log('Code', code);
-    let authenticated = false;
     let upstream:net.Socket;
+    let authenticated = false;
 
     function connect_to_upstream() {
         upstream = net.createConnection(process.argv[2]); // node server.js FIRST_ACTUAL_ARG
@@ -54,14 +90,22 @@ wss.on('connection', (ws: WebSocket) => {
         console.log('message', msg);
         // maybe implement as JSON RPC too?
         if (msg.type === 'auth') {
-            if (msg.code === code) {
-                ws.send(JSON.stringify({'type': 'auth_ok'}) + "\n");
-                authenticated = true;
-                connect_to_upstream();
+            if (canAuthenticate) {
+                if (msg.code === secret) {
+                    ws.send(JSON.stringify({'type': 'auth_ok'}) + "\n");
+                    authenticated = true;
+                    canAuthenticate = false;
+                    connect_to_upstream();
+                } else {
+                    ws.send(JSON.stringify({'type': 'auth_fail', 'hint': 'wrong secret'}) + "\n");
+                    authAttempts++;
+                    assert.ok(authAttempts < 10, 'Too many auth attempts');
+                }
+                return;
             } else {
-                ws.send(JSON.stringify({'type': 'auth_fail'}) + "\n");
+                ws.send(JSON.stringify({'type': 'auth_fail', 'hint': 'too late'}) + "\n");
+                return;
             }
-            return;
         }
         if (!authenticated) {
             ws.send(JSON.stringify({'type': 'please_auth', 'hint': 'forbidden'}) + "\n");
